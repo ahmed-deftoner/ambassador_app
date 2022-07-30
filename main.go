afef99ee331815500dbfdeb7cdbdb2f2bdd7dc71
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-30 22:54:30 +0500

    connect to rds

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	dsn := "root:root@tcp(db:3306)/ambassador"
+	dsn := "admin:Ghtwhts786@tcp(database-1.ckpee9qqxzv9.ap-south-1.rds.amazonaws.com:3306)/db"
 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to mysql")
