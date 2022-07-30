250ea7cc0366b86ceb7627cc73a4e415875a4a88
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-30 16:06:56 +0500

    mysql connection

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	dsn := "root:ahmed@tcp(127.0.0.1:3306)/ambassador?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to mysql")
+	}
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
