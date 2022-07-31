8a92d511c391a6a1c4f9a4348bd4c7e357b1bde5
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-31 19:26:40 +0500

    authcontroller init

--- pkg/routes/route.go
+++ pkg/routes/route.go
@@ -1,6 +1,9 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/ahmed-deftoner/ambassador/pkg/controllers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Setup(app fiber.App) {
 	api := app.Group("api")
