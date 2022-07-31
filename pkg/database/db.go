9f77d3fb80b07517c1dbabcd97523865a5bccc24
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-31 17:10:07 +0500

    auto migrate

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,14 +1,22 @@
 package database
 
 import (
+	"github.com/ahmed-deftoner/ambassador/src/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
+	var err error
 	dsn := "admin:Ghtwhts786@tcp(database-1.ckpee9qqxzv9.ap-south-1.rds.amazonaws.com:3306)/db"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to mysql")
 	}
+}
+
+func AddMigration() {
+	DB.AutoMigrate(models.User{})
 }
