fade16dee8fe6aba43582cbfd3d26fd4da3d72ea
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-31 19:24:14 +0500

    routes pkg

---
+++ pkg/routes/route.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func Setup(app fiber.App) {
+	api := app.Group("api")
+	admin := api.Group("admin")
+	admin.Post("register", controllers.Register)
+}
