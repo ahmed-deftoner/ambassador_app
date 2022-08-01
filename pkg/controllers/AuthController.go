23fb83f32d585ca18181f28cae9219475ff89c1f
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-01 17:04:12 +0500

    register func

--- pkg/controllers/AuthController.go
+++ pkg/controllers/AuthController.go
@@ -1,8 +1,30 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/ahmed-deftoner/ambassador/pkg/models"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Register(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["confirmPassword"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "password confirmation failed",
+		})
+	}
+
+	user := models.User{
+		FirstName:    data["firstName"],
+		LastName:     data["lastName"],
+		Email:        data["email"],
+		IsAmbassador: false,
+	}
 	return c.JSON(fiber.Map{
 		"message": "hello",
 	})
