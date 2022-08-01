3a35c11fa5c327b36e73ab1f6095bf1e0ef06e69
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-01 17:09:33 +0500

    register complete

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.38.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	gorm.io/driver/mysql v1.3.5 // indirect
 	gorm.io/gorm v1.23.8 // indirect
--- pkg/controllers/AuthController.go
+++ pkg/controllers/AuthController.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"github.com/ahmed-deftoner/ambassador/pkg/database"
 	"github.com/ahmed-deftoner/ambassador/pkg/models"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -19,13 +21,17 @@ func Register(c *fiber.Ctx) error {
 		})
 	}
 
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 4)
+
 	user := models.User{
 		FirstName:    data["firstName"],
 		LastName:     data["lastName"],
 		Email:        data["email"],
+		Password:     string(password),
 		IsAmbassador: false,
 	}
-	return c.JSON(fiber.Map{
-		"message": "hello",
-	})
+
+	database.DB.Create(&user)
+
+	return c.JSON(user)
 }
