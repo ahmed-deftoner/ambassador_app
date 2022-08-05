59fbc0c24825e214688cd9d59c4a8aaf04ce7c50
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-08-05 21:56:20 +0500

    cors

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"github.com/ahmed-deftoner/ambassador/pkg/database"
 	"github.com/ahmed-deftoner/ambassador/pkg/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 	database.AddMigration()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 	routes.Setup(app)
 
 	app.Listen(":8000")
